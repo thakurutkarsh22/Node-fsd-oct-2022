c14b2511dcc29856a396f6921303455edecd50e3
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-11-08 00:01:17 +0530

    authentication bug solve

--- ExpressAppCurrencyDemo/middlewares/verifyAuth.js
+++ ExpressAppCurrencyDemo/middlewares/verifyAuth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function verifyAuth(req, res, next) {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req?.headers?.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Auth Failed!!!" });
